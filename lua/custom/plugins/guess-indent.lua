ad320683419965ee935ef636f612493c9fc4fb42
Sam Ievins <sievins8012@gmail.com>
2025-12-28 20:48:25 +0000

    Move guess-indent plugin

---
+++ lua/custom/plugins/guess-indent.lua
@@ -0,0 +1,6 @@
+-- Match the indentation styles used in the current buffer
+-- https://github.com/NMAC427/guess-indent.nvim
+
+return {
+  'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
+}
