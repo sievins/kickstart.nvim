02ddcbf857ae673024ced672d98f61a10921e25b
Sam Ievins <sievins8012@gmail.com>
2025-12-29 15:49:49 +0000

    Resize splits when window is resized

--- lua/sievins/config/autocmds.lua
+++ lua/sievins/config/autocmds.lua
@@ -1,6 +1,20 @@
 -- [[ Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
+local function augroup(name)
+  return vim.api.nvim_create_augroup('sievins_' .. name, { clear = true })
+end
+
+-- Resize splits if window got resized
+vim.api.nvim_create_autocmd({ 'VimResized' }, {
+  group = augroup 'resize_splits',
+  callback = function()
+    local current_tab = vim.fn.tabpagenr()
+    vim.cmd 'tabdo wincmd ='
+    vim.cmd('tabnext ' .. current_tab)
+  end,
+})
+
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.hl.on_yank()`
