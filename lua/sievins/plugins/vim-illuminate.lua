d75b802e52eb75a74f39ebbcbcf2744d81957be2
Sam Ievins <sievins8012@gmail.com>
2026-01-03 22:21:16 +0000

    Augroup

--- lua/sievins/plugins/vim-illuminate.lua
+++ lua/sievins/plugins/vim-illuminate.lua
@@ -32,6 +32,7 @@ return {
 
     -- also set it after loading ftplugins, since a lot overwrite [[ and ]]
     vim.api.nvim_create_autocmd('FileType', {
+      group = vim.api.nvim_create_augroup('illuminate_keymaps', { clear = true }),
       callback = function()
         local buffer = vim.api.nvim_get_current_buf()
         map(']]', 'next', buffer)
