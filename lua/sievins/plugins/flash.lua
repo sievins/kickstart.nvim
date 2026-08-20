b1b49dcc48501b1a4d913f35a7b62e48d9501834
Sam Ievins <sievins8012@gmail.com>
2026-08-20 13:13:25 +0100

    Document flash

--- lua/sievins/plugins/flash.lua
+++ lua/sievins/plugins/flash.lua
@@ -6,12 +6,15 @@ return {
   opts = {},
   -- stylua: ignore
   keys = {
+    -- Jump anywhere: type chars, pick a label. Also works mid-operator (e.g. ys<label>).
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    -- Label the treesitter nodes around the cursor, pick one to select it (yS<label> yanks it).
     { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    -- Operate elsewhere: yr -> jump to a label -> finish with a motion (iw, $, ...) -> cursor returns.
     { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-    -- Simulate nvim-treesitter incremental selection
+    -- Operate on a remote node: yR -> search -> pick a node label -> yanked, cursor returns.
+    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search { remote_op = { motion = true } } end, desc = "Treesitter Search" },
+    -- Select the node under the cursor, then <c-space> grows / <BS> shrinks the selection.
     { "<c-space>", mode = { "n", "o", "x" },
       function()
         require("flash").treesitter({
