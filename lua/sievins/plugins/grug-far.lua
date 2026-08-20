d9f35b1400d669b0b131b2b9ca3ff93d0873baeb
Sam Ievins <sievins8012@gmail.com>
2026-08-20 13:50:36 +0100

    Add grug-far

---
+++ lua/sievins/plugins/grug-far.lua
@@ -0,0 +1,23 @@
+-- Project-wide search and replace
+-- https://github.com/MagicDuck/grug-far.nvim
+
+return {
+  'MagicDuck/grug-far.nvim',
+  cmd = 'GrugFar',
+  opts = { headerMaxWidth = 80 },
+  keys = {
+    {
+      '<leader>sr',
+      function()
+        local grug = require 'grug-far'
+        local ext = vim.bo.buftype == '' and vim.fn.expand '%:e'
+        grug.open {
+          transient = true,
+          prefills = { filesFilter = ext ~= '' and '*.' .. ext or nil },
+        }
+      end,
+      mode = { 'n', 'v' },
+      desc = 'Search and Replace',
+    },
+  },
+}
