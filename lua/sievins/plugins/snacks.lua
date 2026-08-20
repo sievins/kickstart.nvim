4aac93d50c32fb7952ab607bd59de9a5284a6f14
Sam Ievins <sievins8012@gmail.com>
2026-08-20 10:30:53 +0100

    Add terminal toggle

--- lua/sievins/plugins/snacks.lua
+++ lua/sievins/plugins/snacks.lua
@@ -454,6 +454,24 @@ return {
       desc = 'Toggle Zen Mode',
     },
 
+    -- Terminal
+    {
+      '<c-/>',
+      function()
+        Snacks.terminal()
+      end,
+      desc = 'Toggle Terminal',
+      mode = { 'n', 't' },
+    },
+    {
+      '<c-_>', -- what many terminals actually send for ctrl+slash
+      function()
+        Snacks.terminal()
+      end,
+      desc = 'which_key_ignore',
+      mode = { 'n', 't' },
+    },
+
     -- Scratch
     {
       '<leader>.',
