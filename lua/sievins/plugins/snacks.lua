377865d0c357fd5bbd5e5b0b02f750d3367870e2
Sam Ievins <sievins8012@gmail.com>
2026-08-20 10:01:24 +0100

    Remove duplicate leader-sb keymap

--- lua/sievins/plugins/snacks.lua
+++ lua/sievins/plugins/snacks.lua
@@ -243,13 +243,6 @@ return {
       desc = 'Autocmds',
     },
     {
-      '<leader>sb',
-      function()
-        Snacks.picker.lines()
-      end,
-      desc = 'Buffer Lines',
-    },
-    {
       '<leader>sc',
       function()
         Snacks.picker.command_history()
