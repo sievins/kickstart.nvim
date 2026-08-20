de4a8c8abc6f6d7de40c0d966f7cc5757786815b
Sam Ievins <sievins8012@gmail.com>
2026-08-20 08:26:43 +0100

    Remove comments

--- init.lua
+++ init.lua
@@ -281,15 +281,7 @@ require('lazy').setup({
   -- require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    This is the easiest way to modularize your config.
-  --
-  --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   { import = 'sievins.plugins' },
-  --
-  -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
-  -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
-  -- you can continue same window with `<space>sr` which resumes last search
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
