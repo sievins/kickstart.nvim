a45ce37370eb45e5d93fce283a1a35ff84a6578b
Sam Ievins <sievins8012@gmail.com>
2026-08-20 12:01:06 +0100

    Enable tailwind colours in nvim-highlight-colors

--- lua/sievins/plugins/nvim-highlight-colors.lua
+++ lua/sievins/plugins/nvim-highlight-colors.lua
@@ -1,9 +1,7 @@
 return {
   'brenoprata10/nvim-highlight-colors',
-
-  config = function(_, opts)
-    require('nvim-highlight-colors').setup(opts)
-  end,
-
-  -- TODO: csp and lsp integration
+  opts = {
+    render = 'background',
+    enable_tailwind = true,
+  },
 }
