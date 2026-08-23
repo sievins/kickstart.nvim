c69cd4d5ed95cc32a56b3821eb5656ed79369239
Sam Ievins <sievins8012@gmail.com>
2026-08-23 21:30:23 +0100

    Fix update ref on file move/rename

--- lua/sievins/plugins/neo-tree.lua
+++ lua/sievins/plugins/neo-tree.lua
@@ -211,6 +211,17 @@ return {
     },
 
     config = function(_, opts)
+      -- Update LSP references on file move/rename
+      local function on_move(data)
+        Snacks.rename.on_rename_file(data.source, data.destination)
+      end
+      local events = require 'neo-tree.events'
+      opts.event_handlers = opts.event_handlers or {}
+      vim.list_extend(opts.event_handlers, {
+        { event = events.FILE_MOVED, handler = on_move },
+        { event = events.FILE_RENAMED, handler = on_move },
+      })
+
       require('neo-tree').setup(opts)
 
       -- Make neo-tree background match the rest of Neovim
@@ -226,17 +237,6 @@ return {
             require('neo-tree.sources.git_status').refresh()
           end
         end,
-      })
-
-      -- Update LSP references on file move/rename
-      local function on_move(data)
-        Snacks.rename.on_rename_file(data.source, data.destination)
-      end
-      local events = require 'neo-tree.events'
-      opts.event_handlers = opts.event_handlers or {}
-      vim.list_extend(opts.event_handlers, {
-        { event = events.FILE_MOVED, handler = on_move },
-        { event = events.FILE_RENAMED, handler = on_move },
       })
     end,
   },
